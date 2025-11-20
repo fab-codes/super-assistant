87a2ebf1a007af7ddd28833012a8396827e1cb1b
fab-codes <fabrizio.greco@protonmail.com>
2025-11-20 13:46:31 +0100

    feat: Added missing timezone management

--- src/graph/nodes/init_state_node.py
+++ src/graph/nodes/init_state_node.py
@@ -1,9 +1,10 @@
 import datetime
 import pytz
+from src.config.settings import AppConfig
 from src.graph.state import State
 
 def init_state_node(state: State) -> State:
-    timezone = pytz.timezone('Europe/Rome')
+    timezone = pytz.timezone(AppConfig.TIMEZONE)
     
     state.update({
         "current_time": datetime.datetime.now(timezone)
